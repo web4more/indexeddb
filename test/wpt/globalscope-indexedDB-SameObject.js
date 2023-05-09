da77a08405dc81013255402233da952340299189
Jacob Hummer <jcbhmr@outlook.com>
2023-05-09 20:57:42 +0000

    redownload official wpt as of https://github.com/web-platform-tests/wpt/tree/bf4540b22b7a2a6939d8cf7468c4ce76e48c3fef

--- test/wpt/globalscope-indexedDB-SameObject.js
+++
@@ -1,9 +0,0 @@
-import "../wpt-env.js";
-
-test((t) => {
-    assert_equals(
-        self.indexedDB,
-        self.indexedDB,
-        "Attribute should yield the same object each time",
-    );
-}, "indexedDB is [SameObject]");
