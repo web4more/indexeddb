c76d0c70961cc7944dbdc0b3006dec5f6d355746
Jacob Hummer <jcbhmr@outlook.com>
2023-05-10 01:45:55 +0000

    add comment

--- tools/wpt-IndexedDB-setup.cjs
+++ tools/wpt-IndexedDB-setup.cjs
@@ -2,6 +2,9 @@ let ponyfill;
 import("../dist/ponyfill.js").then((m) => (ponyfill = m));
 
 function setup(window) {
+  // This is a race condition. I know. Right now I don't know of a way to async
+  // import an ESM file when using the wpt-runner. So we just hope the file is
+  // loaded by now! If not, then we have a problem.
   if (!ponyfill) {
     throw new ReferenceError("setup() called before ponyfill was loaded");
   }
